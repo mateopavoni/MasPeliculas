b368b0fc317ce91aa53fe7bcdf84e3842bae0cfc
Mateo Pavoni <mateopavoni905@gmail.com>
2026-02-09 15:59:16 -0300

    feat: agregue la entidad de genero

---
+++ Entidades/Genero.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MasPelículasAPI.Entidades
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string Nombre { get; set; }
+    }
+}
