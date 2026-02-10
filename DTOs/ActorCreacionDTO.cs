5f410b89a96d606b865a615c71eef3fd868cc6d8
Mateo Pavoni <mateopavoni905@gmail.com>
2026-02-09 22:47:21 -0300

    feat: agregue validacion de peso para las imagenes del actor

--- DTOs/ActorCreacionDTO.cs
+++ DTOs/ActorCreacionDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MasPelículasAPI.Validaciones;
+using System.ComponentModel.DataAnnotations;
 
 namespace MasPelículasAPI.DTOs
 {
@@ -8,6 +9,7 @@ namespace MasPelículasAPI.DTOs
         [StringLength(120)]
         public string Nombre { get; set; } = null!;
         public DateTime FechaNacimiento { get; set; }
+        [PesoArchivoValidacion(4)]
         public IFormFile Foto { get; set; }
     }
 }
