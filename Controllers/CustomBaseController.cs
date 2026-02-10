69116687b951ded6d7bf99f095a8067dfc3f9718
Mateo Pavoni <mateopavoni905@gmail.com>
2026-02-10 16:40:08 -0300

    feat: agregue todos los metodos en el CustomBaseController

--- Controllers/CustomBaseController.cs
+++ Controllers/CustomBaseController.cs
@@ -1,8 +1,133 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using MasPelículasAPI.DTOs;
+using MasPelículasAPI.Entidades;
+using MasPelículasAPI.Helpers;
+using MasPelículasAPI.Utilidades; // Necesario para tus extensiones de paginación
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MasPelículasAPI.Controllers
 {
     public class CustomBaseController : ControllerBase
     {
+        protected readonly ApplicationDbContext context;
+        protected readonly IMapper mapper;
+
+        public CustomBaseController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        // GET: Listado general
+        protected async Task<List<TDTO>> Get<TEntidad, TDTO>() where TEntidad : class
+        {
+            var entidades = await context.Set<TEntidad>().AsNoTracking().ToListAsync();
+            return mapper.Map<List<TDTO>>(entidades);
+        }
+
+        // GET: Listado con Paginación
+        protected async Task<List<TDTO>> Get<TEntidad, TDTO>(PaginacionDTO paginacionDTO) where TEntidad : class
+        {
+            var queryable = context.Set<TEntidad>().AsQueryable();
+            await HttpContext.InsertarParametrosPaginacion(queryable, paginacionDTO.CantidadRegistrosPorPagina);
+            var entidades = await queryable.Paginar(paginacionDTO).ToListAsync();
+            return mapper.Map<List<TDTO>>(entidades);
+        }
+
+        // GET: Por ID
+        protected async Task<ActionResult<TDTO>> Get<TEntidad, TDTO>(int id) where TEntidad : class, IId
+        {
+            var entidad = await context.Set<TEntidad>().AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+            if (entidad == null)
+            {
+                return NotFound();
+            }
+
+            return mapper.Map<TDTO>(entidad);
+        }
+
+        // POST: Crear
+        protected async Task<ActionResult> Post<TCreacion, TEntidad, TLectura>
+            (TCreacion creacionDTO, string nombreRuta) where TEntidad : class, IId
+        {
+            var entidad = mapper.Map<TEntidad>(creacionDTO);
+            context.Add(entidad);
+            await context.SaveChangesAsync();
+            var dtoLectura = mapper.Map<TLectura>(entidad);
+
+            return CreatedAtRoute(nombreRuta, new { id = entidad.Id }, dtoLectura);
+        }
+
+        // PUT: Actualizar
+        protected async Task<ActionResult> Put<TCreacion, TEntidad>
+            (int id, TCreacion creacionDTO) where TEntidad : class, IId
+        {
+            var entidad = await context.Set<TEntidad>().FirstOrDefaultAsync(x => x.Id == id);
+
+            if (entidad == null)
+            {
+                return NotFound();
+            }
+
+            mapper.Map(creacionDTO, entidad);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: Borrar
+        protected async Task<ActionResult> Delete<TEntidad>(int id) where TEntidad : class, IId
+        {
+            var entidad = await context.Set<TEntidad>().FirstOrDefaultAsync(x => x.Id == id);
+
+            if (entidad == null)
+            {
+                return NotFound();
+            }
+
+            context.Remove(entidad);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // PATCH: Actualización Parcial
+        protected async Task<ActionResult> Patch<TEntidad, TPatchDTO>
+            (int id, JsonPatchDocument<TPatchDTO> patchDocument)
+            where TPatchDTO : class
+            where TEntidad : class, IId
+        {
+            if (patchDocument == null)
+            {
+                return BadRequest();
+            }
+
+            var entidad = await context.Set<TEntidad>().FirstOrDefaultAsync(x => x.Id == id);
+
+            if (entidad == null)
+            {
+                return NotFound();
+            }
+
+            var entidadDTO = mapper.Map<TPatchDTO>(entidad);
+
+            patchDocument.ApplyTo(entidadDTO, ModelState);
+
+            var esValido = TryValidateModel(entidadDTO);
+
+            if (!esValido)
+            {
+                return BadRequest(ModelState);
+            }
+
+            mapper.Map(entidadDTO, entidad);
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
