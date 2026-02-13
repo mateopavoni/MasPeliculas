02d89fbd427736762f98d2e9cbd8df24fbecf8aa
Mateo Pavoni <mateopavoni905@gmail.com>
2026-02-13 00:23:19 -0300

    chore: reestructurar proyecto a solucion con MasPelículasAPI y MasPelículas.Test

---
+++ MasPelículas.Test/BasePruebas.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using MasPelículasAPI;
+using MasPelículasAPI.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite; // Necesario para NtsGeometryServices
+using NetTopologySuite.Geometries;
+using System;
+using System.Security.Claims;
+
+namespace MasPeliculas.Tests
+{
+    public class BasePruebas
+    {
+        protected ApplicationDbContext ConstruirContext(string nombreDB)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(nombreDB)
+                .Options;
+
+            var dbContext = new ApplicationDbContext(options);
+            return dbContext;
+        }
+
+        protected IMapper ConfigurarAutoMapper()
+        {
+            var config = new MapperConfiguration(options =>
+            {
+                var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+                options.AddProfile(new AutoMapperProfiles(geometryFactory));
+            });
+
+            return config.CreateMapper();
+        }
+
+        protected void ConstruirControllerUsuario(ControllerBase controller, string idUsuario)
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, idUsuario),
+                new Claim(ClaimTypes.Email, "ejemplo@prueba.com"),
+                new Claim(ClaimTypes.Name, "usuario_prueba")
+            }, "TestAuthentication"));
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+        }
+    }
+}
---
+++ MasPelículas.Test/PruebasUnitarias/GeneroControllerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasPelículas.Tests.PruebasUnitarias
+{
+    internal class GeneroControllerTests
+    {
+    }
+}
